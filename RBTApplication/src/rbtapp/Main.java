47fd3b82b2013d4025c39bc332ef7c792bb3ff80
Djordje <Djordje@Asus>
2019-03-13 01:32:45 +0100

    First night

--- RBTApplication/src/rbtapp/Main.java
+++ RBTApplication/src/rbtapp/Main.java
@@ -1,9 +1,33 @@
 package rbtapp;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String fileInput = args[0];
+		try {
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(new File(fileInput));
+			
+			doc.getDocumentElement().normalize();
+			
+			NodeList cellList = doc.getElementsByTagName("cells");
+			
+			Node cell = cellList.item(0);
+			NodeList cellChield = cell.getChildNodes();
+			System.out.println(cellChield.getLength());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
